0070df0229e7b2809797c5319ac7ed651ffc95c0
cjhgit <1418503647@qq.com>
2026-04-22 18:56:12 +0800

    Update version to v26.422.2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function createApp() {
   });
 
   router.get("/version", (ctx) => {
-    ctx.body = "v26.422.1";
+    ctx.body = "v26.422.2";
   });
 
   app.use(router.routes());
--- test/version.test.js
+++ test/version.test.js
@@ -3,9 +3,9 @@ const assert = require("node:assert/strict");
 const request = require("supertest");
 const { createApp } = require("../app");
 
-test("GET /version returns v26.422.1", async () => {
+test("GET /version returns v26.422.2", async () => {
   const app = createApp();
 
   const res = await request(app.callback()).get("/version").expect(200);
-  assert.strictEqual(res.text, "v26.422.1");
+  assert.strictEqual(res.text, "v26.422.2");
 });
